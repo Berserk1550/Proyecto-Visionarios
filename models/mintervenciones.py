56dd204254a964e17770142f606b8b64f2387237
LopEmiJuspian <elimy2003@gmail.com>
2026-02-06 01:54:12 -0500

    COMPROMISOS CON FECHAS

--- models/mintervenciones.py
+++ models/mintervenciones.py
@@ -2,15 +2,19 @@ import mysql.connector
 from datetime import datetime
 
 # Guardar nueva intervención
-def nueva_intervencion(num_caso, documento, doc_pronal, descripcion, compromiso):
+def nueva_intervencion(num_caso, documento, doc_pronal, descripcion, compromiso=None, fecha_compromiso=None):
     conexion = mysql.connector.connect(host="localhost", user="root", password="", database="visionarios")
     cursor = conexion.cursor()
-    sql = """INSERT INTO intervenciones
-         (num_caso, doc_pronal, descripcion,
-         int_compromiso, int_fecha_compromiso, int_estado_compromiso, int_estado, fecha, fecha_registro)
-         VALUES (%s,%s,%s,%s,%s,%s,'activa',%s,%s)"""
-    valores = ( num_caso, doc_pronal, descripcion, compromiso, datetime.now().strftime("%Y-%m-%d"),"Pendiente", datetime.now().strftime("%Y-%m-%d"), datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    
+
+    if compromiso and fecha_compromiso:
+        sql = """INSERT INTO intervenciones
+             (num_caso, doc_pronal, descripcion,
+             int_compromiso, int_fecha_compromiso, int_estado_compromiso, int_estado, fecha, fecha_registro)
+             VALUES (%s,%s,%s,%s,%s,%s,'activa',%s,%s)"""
+        valores = ( num_caso, doc_pronal, descripcion, compromiso, fecha_compromiso,"Pendiente", datetime.now().strftime("%Y-%m-%d"), datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    else:
+        sql= """INSERT INTO intervenciones(num_caso, doc_pronal, descripcion, int_estado, fecha, fecha_registro) VALUES (%s,%s,%s,'activa',%s,%s)"""
+        valores = ( num_caso, doc_pronal, descripcion, datetime.now().strftime("%Y-%m-%d"), datetime.now().strftime("%Y-%m-%d %H:%M:%S") )
     cursor.execute(sql, valores)
     conexion.commit()
     cursor.close()
@@ -24,4 +28,4 @@ def obtener_intervenciones(num_caso):
     intervenciones = cursor.fetchall()
     cursor.close()
     conexion.close()
-    return intervenciones
+    return intervenciones 
